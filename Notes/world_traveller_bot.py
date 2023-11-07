bb8e4291eb9b48c90306ccdac18fb0dd3ca8dbf6
Jason Cheng <jasonbobo112@gmail.com>
2023-11-07 13:57:22 -0800

    Added

---
+++ Notes/world_traveller_bot.py
@@ -0,0 +1,18 @@
+user_name = input("What's your name?\n")
+print(f"Nice to meet you, {user_name}!")
+
+continents = ["Asia", "Europe", "North America", "South America", "Australia", "Africa", "Antarctica"]
+visited_continents = 0
+total_continents = 7
+
+for continent in continents:
+    response = input(f"Have you been to {continent}? (yes/no)\n")
+    if response == 'yes':
+            visited_continents += 1
+    else:
+        print("Please enter 'yes' or 'no' as a response.")
+
+print(f"I see, you've visited {visited_continents}/{total_continents} continents!")
+
+
+
